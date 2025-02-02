f19d3b9a741d422db33fa0e644019adafae6697e
abid <abid35-757@diu.edu.bd>
2025-02-02 16:31:13 +0600

    Modified components

--- src/components/navLinks.ts
+++
@@ -1,6 +0,0 @@
-export const navLinks = [
-  { name: "Home", href: "/" },
-  { name: "About", href: "/about" },
-  { name: "Contact", href: "/contact" },
-  { name: "Services", href: "/services" }, // You can add or remove links easily
-];
