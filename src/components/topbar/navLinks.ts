7a223326a3f2eeab4e729388977a9b033967c263
abid <abid35-757@diu.edu.bd>
2025-02-02 16:32:13 +0600

    created a folder in components

---
+++ src/components/topbar/navLinks.ts
@@ -0,0 +1,6 @@
+export const navLinks = [
+  { name: "Home", href: "/" },
+  { name: "About", href: "/about" },
+  { name: "Contact", href: "/contact" },
+  { name: "Services", href: "/services" }, // You can add or remove links easily
+];
